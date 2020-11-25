bb9b58e447522eef68fbfc24c7243af1a10992c9
Jonathan Barone <johnito.barone@gmail.com>
2020-11-25 16:05:38 +0800

    new interface

--- routes/youtube.js
+++ routes/youtube.js
@@ -1,21 +1,40 @@
 const express = require('express');
-const YoutubeStream = require('../ytstream');
+const ytdl = require('ytdl-core')
+const FFmpeg = require('fluent-ffmpeg')
+const { PassThrough } = require('stream')
+const fs = require('fs')
 
 youtubeRouter = express.Router();
 
 youtubeRouter.get('/stream', (req, res) => {
-    const requestUrl = `http://youtube.com/watch?v=${req.query.videoId}`
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    try {
-      YoutubeStream(requestUrl).pipe(res)
-    } catch (exception) {
-      res.status(500).send(exception)
-    }
-    // process.on('unhandledRejection', error => {
-    //   // Prints "unhandledRejection woops!"
-    //   res.status(500).send(error)
-    // });
-});
+        const requestUrl = `http://youtube.com/watch?v=${req.query.videoId}`
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        opt = {
+            audioFormat: 'mp3',
+            quality: 'lowestaudio'
+        }
+        const video = ytdl(requestUrl, opt)
+        const { file, audioFormat } = opt
+        let stream = file ? fs.createWriteStream(file) : new PassThrough()
+        const ffmpeg = new FFmpeg(video)
+        process.nextTick(() => {
+          const output = ffmpeg.format(audioFormat).pipe(stream)
+          ffmpeg.on('error', error => {
+              stream.emit('error', error)
+              video.end()
+              console.log('Error with video ', req.query.videoId, error.message)
+              res.status(500).send(error.message);
+          })
+        //   output.on('error', error => {
+        //     video.end()
+        //     stream.emit('error', error)
+        //     reject(stream)
+        //   })
+        })
 
+        stream.video = video
+        stream.ffmpeg = ffmpeg
+        stream.pipe(res)
+    });
 
 module.exports = youtubeRouter;
