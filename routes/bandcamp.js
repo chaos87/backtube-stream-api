788b17b3e793e5879061877b1e1a957b6b32466d
Jonathan Barone <johnito.barone@gmail.com>
2021-01-26 14:35:34 +0800

    added Dockerfile

--- routes/bandcamp.js
+++ routes/bandcamp.js
@@ -3,8 +3,7 @@ const cors = require('cors');
 // const bandcamp = require('bandcamp-scraper');
 const bandcamp = require('../bandcamp');
 const bandcampStream = require('../bandcamp/stream');
-
-const promisify = f => (...args) => new Promise((a,b)=>f(...args, (err, res) => err ? b(err) : a(res)));
+const { promisify } = require('util');
 
 const getAlbumInfo = promisify(bandcamp.getAlbumInfo);
 const getAlbumUrls = promisify(bandcamp.getAlbumUrls);
@@ -16,33 +15,6 @@ var corsOptions = {
   origin: process.env.CORS_DOMAIN_NAME,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-
-bandcampRouter.post('/search', function (req, res) {
-    return bandcampSearch(req.body).then(
-        response => {
-            res.send(response)
-        }
-    )
-    .catch(e => res.status(400).send(e.stack))
-})
-
-bandcampRouter.post('/albums', function (req, res) {
-    return getAlbumUrls(req.body.url).then(
-        response => {
-            res.send(response)
-        }
-    )
-    .catch(e => res.status(400).send(e.stack))
-})
-
-bandcampRouter.post('/songs', function (req, res) {
-    return getAlbumInfo(req.body.url).then(
-        response => {
-            res.send(response)
-        }
-    )
-    .catch(e => res.status(400).send(e.stack))
-  })
 
 bandcampRouter.get('/stream', cors(corsOptions), function (req, res) {
     bandcampStream.getTrack(req.query.url).then(function(stream) {
